633e8c8ab1e0fa5510ad3b70096405e0f836dd32
jinnychoe <125236103+jinnychoe@users.noreply.github.com>
2023-02-28 17:29:40 -0800

    Update and rename ClassExPart2-1.py to ClassExPart2-Chal1.py

--- ClassExPart2-Chal1.py
+++ ClassExPart2-Chal1.py
@@ -1,18 +1,23 @@
 #Jinny Choe
 #2/27/2023
-#Class Exercise Week 3 Part 2 Question 1:
+#Class Exercise Week 3 Part 2 Challenge 1: Sum of all numbers
 
-MAX = 5
+MAX = 5 #max number
 
+#initialize var
 total = 0.0
+
 print ('This program calculates the sum of')
 print (f'{MAX} numbers you will enter')
 
+#get user input & accumulate them
 for counter in range(MAX):
     number = int(input('Enter a number: '))
     total = total + number
-
+    
+#calculate average
 avg = total / MAX
 
+#prints results
 print(f'The total is {total}.')
 print(f'The average is {avg}.')
