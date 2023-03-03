8480af3c8000d97cb4ab53779171faf7139d4b3a
jinnychoe <125236103+jinnychoe@users.noreply.github.com>
2023-03-02 19:58:56 -0800

    Update ClassExPart2-Chal4.py

--- ClassExPart2-Chal4.py
+++ ClassExPart2-Chal4.py
@@ -1,3 +1,5 @@
+
+
 #Jinny Choe
 #2/27/2023
 #Class Exercise Week 3 Part 2 Challenge 4: Calories burned
@@ -7,7 +9,7 @@ callist = [] #initializes calories burned per minute list
 calpermin = 4.2 #sets calories per minute burned multiplier
 
 #print results
-print ("Minutes exercised      Calories burned")
+print ("Minutes exercised\tCalories burned")
 for n in range(len(minlist)): #loops through each item in minutes list
     callist.append(calpermin*minlist[n]) #adds calories burned per minute to calorie list
-    print(f"{minlist[n]}                     {callist[n]:.1f}") # prints minutes and calorie list
+    print(f"{minlist[n]}\t\t\t{callist[n]:.1f}") # prints minutes and calorie list
